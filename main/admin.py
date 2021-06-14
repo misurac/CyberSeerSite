a61f4bd211e57a725156c09fd010a1df488e1b7c
misurac <rmisurac@gmail.com>
2021-06-14 11:44:02 -0700

    attempted to include tinymce4-lite app. no error but did not succeed in implementation

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Tutorial
+from tinymce.widgets import TinyMCE
+from django.db import models
+
 # Register your models here.
 
 class TutorialAdmin(admin.ModelAdmin):
@@ -7,5 +10,9 @@ class TutorialAdmin(admin.ModelAdmin):
 		("Title/Date", {"fields": ["tutorial_title", "tutorial_published"]}),
 		("Content", {"fields": ["tutorial_content"]})
 	]
+
+	formfields_overrides = {
+		models.TextField: {'widget': TinyMCE()}
+	}
 
 admin.site.register(Tutorial, TutorialAdmin)
