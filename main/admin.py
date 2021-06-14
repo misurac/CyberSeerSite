d5d2bba79b3ef0a47fe6fa125b6fdda7ce66b193
misurac <rmisurac@gmail.com>
2021-06-14 11:23:19 -0700

    small updates to tutorials model

--- main/admin.py
+++ main/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Tutorial
 # Register your models here.
 
-admin.site.register(Tutorial)
+class TutorialAdmin(admin.ModelAdmin):
+	fieldsets = [
+		("Title/Date", {"fields": ["tutorial_title", "tutorial_published"]}),
+		("Content", {"fields": ["tutorial_content"]})
+	]
+
+admin.site.register(Tutorial, TutorialAdmin)
